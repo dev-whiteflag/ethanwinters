47697b76db91e60f5f016efe49eaa657cf97dc0a
Brenno Fagundes (imwhiteflag) <dev.whiteflag@gmail.com>
2022-06-23 19:05:46 -0300

    fix: command file was not being found

--- README.md
+++ README.md
@@ -1,11 +1,16 @@
 
 # Ethan Winters `discord bot`
 
+## Intents
+
+- `Intents.FLAGS.GUILDS`
+- `Intents.FLAGS.GUILD_MESSAGES`
+- `Intents.FLAGS.DIRECT_MESSAGES`
+
 ## Environment Variables
 
 - `EW_API_PORT` 
 - `EW_DISCORD_TOKEN`
 - `EW_S3_REGION`
-- `EW_S3_BUCKET` (temporary)
-- `EW_DISCORD_APP_ID` (temporary)
-- `EW_DISCORD_APP_GUILD_ID` (temporary)
+- `EW_S3_BUCKET`
+- `EW_DB_URL`
--- src/events/message.event.js
+++ src/events/message.event.js
@@ -4,12 +4,13 @@ module.exports = {
     async bind(client) {
         client.on('messageCreate', async message => {
             if (message.content.startsWith('!ew')) {
+                const absolutePath = './src/commands';
                 const relativePath = '../commands';
                 const command = message.content.split(' ')[1];
-                const path = `${relativePath}/${command}.command.js`;
+                const path = `${absolutePath}/${command}.command.js`;
 
                 if (fs.existsSync(path)) {
-                    const clazz = require(path);
+                    const clazz = require(`${relativePath}/${command}.command`);
                     clazz.run(message);
                 } else {
                     message.reply('tem esse comando n ow esquisofrenico');
